f7a866858f63239e0940e3b14e25aacca73ab1e4
Clara Balzano <clarabzo@hotmail.fr>
2022-01-02 15:40:12 +0100

    merge and clean

--- Temple_Fun/TP_projet/main.cpp
+++ Temple_Fun/TP_projet/main.cpp
@@ -6,7 +6,6 @@
 #include FT_FREETYPE_H
 #include <string.h>
 
-
 #include <glimac/SDLWindowManager.hpp>
 #include <glimac/Program.hpp>
 #include <glimac/FilePath.hpp>
@@ -24,7 +23,6 @@
 #include <rendering/Cube.hpp>
 #include <rendering/Text.hpp>
 #include <rendering/Menu.hpp>
-#include <rendering/Model.hpp>
 #include <rendering/Cursor.hpp>
 #include <rendering/json.hpp>
 #include <rendering/Score.hpp>
@@ -160,7 +158,6 @@ int main(int argc, char **argv)
     glUniformMatrix4fv(glGetUniformLocation(TextProgram.getId(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
     glEnable(GL_DEPTH_TEST);
-
 
     // Creation of the Skybox
     rendering::Skybox skybox;
@@ -351,11 +348,9 @@ int main(int argc, char **argv)
              *********************************/
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            
 
-            glm::mat4 ProjMatrix = glm::perspective(glm::radians(70.f), 2000.f / 1000.f, 0.1f, 100.f);
+            glm::mat4 ProjMatrix = glm::perspective(glm::radians(70.f), 1700.f / 900.f, 0.1f, 100.f);
             glm::mat4 NormalMatrix = glm::transpose(glm::inverse(ViewMatrix));
-
 
             // Drawing of the different elements
             light.drawDirectionnal(camera,LightProgram, ProjMatrix, NormalMatrix);
@@ -365,7 +360,7 @@ int main(int argc, char **argv)
             courseMap.drawObstacle(cube_obstacle, camera, LightProgram, ProjMatrix, windowManager);         
             light.drawPonctual(camera,LightProgram,ProjMatrix,NormalMatrix, player.convertCoord());
 
-            //Drawing of the skybox
+
             glDepthFunc(GL_LEQUAL);
             SkyboxProgram.use();
             skybox.draw(SkyboxProgram, camera, ProjMatrix, cubemapTexture);
